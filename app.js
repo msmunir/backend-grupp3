0dd8c5938adb846006a5188467e1936d5d0b72ee
Vüsal Tagiev <88597801+vusaltagiev@users.noreply.github.com>
2023-04-03 23:40:22 +0200

    app

--- app.js
+++ app.js
@@ -2,24 +2,14 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-
 //Middleware
-
-app.use(cors())
-app.use(express.urlencoded({extended:false}))
-app.use(express.json())
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 //Controller
-
-app.use('/api/cases', require('./controllers/caseController'))
-
-app.use('/api/comments', require('./controllers/commentController.js'))
-
-app.use('/api/statuses', require('./controllers/statusController'))
-
+app.use('/api/cases', require('./controllers/caseController'));
+app.use('/api/comments', require('./controllers/commentController.js'));
+app.use('/api/statuses', require('./controllers/statusController'));
 
 module.exports = app;
-
-
-
-
