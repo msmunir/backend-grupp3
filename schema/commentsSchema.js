1b99df75185256400ab8cf0f3449e15b1b3f9e40
Lotta <charlotte.a.nyman@gmail.com>
2023-04-04 15:26:45 +0200

    everything is done

--- schema/commentsSchema.js
+++ schema/commentsSchema.js
@@ -6,7 +6,7 @@ const commentSchema = new mongoose.Schema({
     caseId: {type: mongoose.Schema.Types.ObjectId, ref: "Case"},
     email:{ type: String},
     message:{type: String,}
-})
+}, {timestamps: true})
 
 
 module.exports = mongoose.model("comment", commentSchema)
